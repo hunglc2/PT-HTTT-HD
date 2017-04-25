996822a85d8f32f771f8494e4ec23abb9b5c37fd
Hoan <hoannv@allexceed.co.jp>
2017-04-25 16:49:35 +0700

    commit config source

--- Source/BankManagement/src/main/java/org/hoannguyen/bm/controller/MainController.java
+++ Source/BankManagement/src/main/java/org/hoannguyen/bm/controller/MainController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	
+	@RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("greeting", "Hello Spring MVC");
+        return "index";
+    }
+	
 	@RequestMapping("/customer")
-    public String hello(Model model) {
+    public String customer(Model model) {
         //model.addAttribute("greeting", "Hello Spring MVC");
         //model.addAttribute("title", "Spring MVC");
         return "customer";
