2a517c68444f103d676b41695e9cdfbe4b954e16
Hoan <hoannv@allexceed.co.jp>
2017-04-26 17:05:07 +0700

    commit login page

--- Source/BankManagement/src/main/java/org/hoannguyen/bm/controller/MainController.java
+++ Source/BankManagement/src/main/java/org/hoannguyen/bm/controller/MainController.java
@@ -9,14 +9,20 @@ public class MainController {
 	
 	@RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("greeting", "Hello Spring MVC");
+        model.addAttribute("title", "Login page");
         return "index";
+    }
+	
+	@RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "Login page");
+        model.addAttribute("title", "Login page");
+        return "login";
     }
 	
 	@RequestMapping("/customer")
     public String customer(Model model) {
-        //model.addAttribute("greeting", "Hello Spring MVC");
-        //model.addAttribute("title", "Spring MVC");
+        model.addAttribute("title", "CRM Model HoanNguyen | Starter");
         return "customer";
     }
 }
